41101c3d600735d427d78246752c455e70c06d13
rdmi-stack <76124485+rdmi-stack@users.noreply.github.com>
2023-10-20 21:13:10 +0000

    test

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,7 @@
 // _app.js or _app.tsx
 import '../styles/globals.css'; // Your global styles
 import '@fortawesome/fontawesome-free/css/all.css'; // Import the FontAwesome CSS
-import './fontawesome'; // Import your FontAwesome configuration
-
+import '../utils/fontawesome';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
---
+++ utils/fontawesome.js
@@ -0,0 +1,6 @@
+// utils/fontawesome.js
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+
+library.add(fas, fab);
