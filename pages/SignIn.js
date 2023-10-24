9fbd679adc41b7337304e8232ac1c3e82a39ad04
rdmi-stack <76124485+rdmi-stack@users.noreply.github.com>
2023-10-24 00:38:40 +0000

    test

--- pages/SignIn.js
+++ pages/SignIn.js
@@ -1,27 +1,28 @@
 import React, { useState } from 'react';
 import { useRouter } from 'next/router';
-import { auth } from '../lib/firebase'; // Import Firebase Auth from your firebase.js
-import { getAuth, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'; // Import Firebase auth functions
-import Header from './Header'; // Replace this with the actual path to your Header component
+import { getAuth, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import Header from './Header';
 
 const SignIn = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [error, setError] = useState(null); // Change the initial state to null
+  const [error, setError] = useState(null);
   const router = useRouter();
-  const authInstance = getAuth(); // Initialize Firebase Auth
+  const authInstance = getAuth();
 
   const signIn = async (e) => {
     e.preventDefault();
     try {
-      await signInWithEmailAndPassword(authInstance, email, password); // Use signInWithEmailAndPassword
+      const userCredential = await signInWithEmailAndPassword(authInstance, email, password);
+      const user = userCredential.user;
+      const idToken = await user.getIdToken();
+      localStorage.setItem('userToken', idToken);
       router.push('/dashboard');
     } catch (error) {
-      // Handle Firebase authentication errors separately
       if (error.code === 'auth/user-not-found') {
         setError('User not found. Please check your email.');
       } else if (error.code === 'auth/wrong-password') {
-        setError('Incorrect password. Please try again.'); // Custom error message for wrong password
+        setError('Incorrect password. Please try again.');
       } else {
         setError('An error occurred. Please try again later.');
       }
@@ -29,9 +30,12 @@ const SignIn = () => {
   };
 
   const googleSignIn = async () => {
-    const provider = new GoogleAuthProvider(); // Initialize GoogleAuthProvider
+    const provider = new GoogleAuthProvider();
     try {
-      await signInWithPopup(authInstance, provider); // Use signInWithPopup
+      const userCredential = await signInWithPopup(authInstance, provider);
+      const user = userCredential.user;
+      const idToken = await user.getIdToken();
+      localStorage.setItem('userToken', idToken);
       router.push('/dashboard');
     } catch (error) {
       setError('An error occurred. Please try again later.');
@@ -41,77 +45,76 @@ const SignIn = () => {
   return (
     <>
       <Header />
-      <section className="relative flex flex-wrap lg:h-screen lg:items-center">
-        <div className="w-full px-4 py-12 sm:px-6 sm:py-16 lg:w-1/2 lg:px-8 lg:py-24">
-          <div className="mx-auto max-w-lg text-center">
-            <h1 className="text-2xl font-bold sm:text-3xl">Welcome Back!</h1>
-            <p className="mt-4 text-gray-500">
-              Sign in to continue to your account.
-            </p>
+    <section className="relative flex flex-wrap lg:h-screen lg:items-center">
+      <div className="w-full px-4 py-12 sm:px-6 sm:py-16 lg:w-1/2 lg:px-8 lg:py-24">
+        <div className="mx-auto max-w-lg text-center">
+          <h1 className="text-2xl font-bold sm:text-3xl">Welcome Back!</h1>
+          <p className="mt-4 text-gray-500">
+            Sign in to continue to your account.
+          </p>
+        </div>
+
+        <form onSubmit={signIn} className="mx-auto mb-0 mt-8 max-w-md space-y-4">
+          <div>
+            <label htmlFor="email" className="sr-only">Email</label>
+            <div className="relative">
+              <input
+                type="email"
+                id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
+                placeholder="Enter email"
+              />
+            </div>
           </div>
 
-          <form onSubmit={signIn} className="mx-auto mb-0 mt-8 max-w-md space-y-4">
-            <div>
-              <label htmlFor="email" className="sr-only">Email</label>
-              <div className="relative">
-                <input
-                  type="email"
-                  id="email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  className="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
-                  placeholder="Enter email"
-                />
-              </div>
+          <div>
+            <label htmlFor="password" className="sr-only">Password</label>
+            <div className="relative">
+              <input
+                type="password"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
+                placeholder="Enter password"
+              />
             </div>
+          </div>
 
-            <div>
-              <label htmlFor="password" className="sr-only">Password</label>
-              <div className="relative">
-                <input
-                  type="password"
-                  id="password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  className="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
-                  placeholder="Enter password"
-                />
-              </div>
-            </div>
+          {error && <p className="text-red-500">{error}</p>}
 
-            {error && <p className="text-red-500">{error}</p>}
+          <div className="flex items-center justify-between">
+            <p className="text-sm text-gray-500">
+              No account?
+              <a className="underline" href="/SignUp">Sign up</a>
+            </p>
 
-            <div className="flex items-center justify-between">
-              <p className="text-sm text-gray-500">
-                No account?
-                <a className="underline" href="/SignUp">Sign up</a>
-              </p>
-
-              <button
-                type="submit"
-                className="inline-block rounded-lg bg-blue-500 px-5 py-3 text-sm font-medium text-white"
-              >
-                Sign in
-              </button>
-            </div>
             <button
-              onClick={googleSignIn}
-              className="inline-block rounded-lg bg-white border border-gray-300 px-5 py-3 text-sm font-medium text-gray-500 mt-4"
+              type="submit"
+              className="inline-block rounded-lg bg-blue-500 px-5 py-3 text-sm font-medium text-white"
             >
-              Sign in with Google
+              Sign in
             </button>
-          </form>
-        </div>
+          </div>
+          <button
+            onClick={googleSignIn}
+            className="inline-block rounded-lg bg-white border border-gray-300 px-5 py-3 text-sm font-medium text-gray-500 mt-4"
+          >
+            Sign in with Google
+          </button>
+        </form>
+      </div>
 
-        <div className="relative h-64 w-full sm:h-96 lg:h-full lg:w-1/2">
-          <img
-            alt="Welcome"
-            src="https://images.unsplash.com/photo-1630450202872-e0829c9d6172?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
-            className="absolute inset-0 h-full w-full object-cover"
-          />
-        </div>
-      </section>
-    </>
+      <div className="relative h-64 w-full sm:h-96 lg:h-full lg:w-1/2">
+        <img
+          alt="Welcome"
+          src="https://images.unsplash.com/photo-1630450202872-e0829c9d6172?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+          className="absolute inset-0 h-full w-full object-cover"
+        />
+      </div>
+    </section>    </>
   );
 };
 
