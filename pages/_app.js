0f7b3701624e023fae71cb2a1b2893666d169d8d
rdmi-stack <76124485+rdmi-stack@users.noreply.github.com>
2023-10-24 00:07:34 +0000

    test

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,10 @@
-// _app.js or _app.tsx
+// _app.js
 import '../styles/globals.css'; // Your global styles
 import '@fortawesome/fontawesome-free/css/all.css'; // Import the FontAwesome CSS
 import '../utils/fontawesome';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,56 +1,64 @@
+import { requireAuth } from '../lib/requireAuth'; // adjust the path as needed
 import { useState, useEffect } from 'react';
 import Sidebar from '../components/dashboard/Sidebar';
 import DaisyUIMenu from '../components/dashboard/DaisyUIMenu';
 import MailForm from '../components/dashboard/MailForm';
 import MailModal from '../components/dashboard/MailModal';
 
-export default function Dashboard() {
-    const [website, setWebsite] = useState('');
-    const [mailContent, setMailContent] = useState('');
-    const [showModal, setShowModal] = useState(false);
-    const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        setTimeout(() => {
-            setLoading(false);
-        }, 1000);
-    }, []);
 
-    const handleGenerateMail = () => {
-        const generatedMail = `Generated mail for ${website}`;
-        setMailContent(generatedMail);
-        setShowModal(true);
-    };
-
-    if (loading) {
-        return (
-            <div className="skeleton-wrapper flex min-h-screen">
-                <div className="skeleton-sidebar"></div>
-                <div className="skeleton-content flex flex-col justify-center items-center">
-                    <div className="skeleton-line"></div>
-                    <div className="skeleton-line"></div>
-                    <div className="skeleton-line"></div>
-                    <div className="skeleton-line"></div>
-                    <div className="skeleton-line"></div>
-                    <div className="skeleton-line"></div>
-                </div>
-            </div>
-        );
-    }
-
-    return (
-        <div className="flex min-h-screen bg-reachly-bg">
-            <Sidebar />
-            <div className="flex-1 flex flex-col">
-                <DaisyUIMenu />
-                <div className="flex-1 p-10 grid grid-cols-12 gap-4">
-                    <div className="col-start-1 col-span-12 bg-white rounded p-4 shadow-md">
-                        <h1 className="text-2xl font-bold mb-4">Personalization System</h1>
-                        <MailForm website={website} setWebsite={setWebsite} onGenerate={handleGenerateMail} />
-                    </div>
-                </div>
-                {showModal && <MailModal mailContent={mailContent} setMailContent={setMailContent} onClose={() => setShowModal(false)} />}
-            </div>
-        </div>
-    );
+export async function getServerSideProps(context) {
+  return await requireAuth(context);
 }
+function Dashboard() {
+  const [website, setWebsite] = useState('');
+  const [mailContent, setMailContent] = useState('');
+  const [showModal, setShowModal] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
+
+  const handleGenerateMail = () => {
+    const generatedMail = `Generated mail for ${website}`;
+    setMailContent(generatedMail);
+    setShowModal(true);
+  };
+
+  if (loading) {
+    return (
+      <div className="skeleton-wrapper flex min-h-screen">
+        <div className="skeleton-sidebar"></div>
+        <div className="skeleton-content flex flex-col justify-center items-center">
+          <div className="skeleton-line"></div>
+          <div className="skeleton-line"></div>
+          <div className="skeleton-line"></div>
+          <div className="skeleton-line"></div>
+          <div className="skeleton-line"></div>
+          <div className="skeleton-line"></div>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex min-h-screen bg-reachly-bg">
+      <Sidebar />
+      <div className="flex-1 flex flex-col">
+        <DaisyUIMenu />
+        <div className="flex-1 p-10 grid grid-cols-12 gap-4">
+          <div className="col-start-1 col-span-12 bg-white rounded p-4 shadow-md">
+            <h1 className="text-2xl font-bold mb-4">Personalization System</h1>
+            <MailForm website={website} setWebsite={setWebsite} onGenerate={handleGenerateMail} />
+          </div>
+        </div>
+        {showModal && <MailModal mailContent={mailContent} setMailContent={setMailContent} onClose={() => setShowModal(false)} />}
+      </div>
+    </div>
+  );
+}
+
+export default Dashboard;
