2f64e0410606b28f8a96a67f2f438c04a67f8651
rdmi-stack <76124485+rdmi-stack@users.noreply.github.com>
2023-10-29 22:10:38 +0000

    test

--- pages/testpage.js
+++ pages/testpage.js
@@ -3,24 +3,33 @@ import { useEffect, useState } from 'react';
 
 const MyComponent = () => {
   const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    // Replace the URL with your Flask API's URL
     const url = 'https://api.elixcent.com/api_endpoint';
 
     axios.get(url)
       .then(response => {
         setData(response.data);
+        setIsLoading(false);
       })
       .catch(error => {
         console.error('There was an error!', error);
+        setIsLoading(false);
       });
   }, []);
+
+  const Skeleton = () => (
+    <div style={{ border: '1px solid #ccc', padding: '16px', borderRadius: '4px'}}>
+      <div style={{ backgroundColor: '#eee', height: '20px', marginBottom: '8px' }}></div>
+      <div style={{ backgroundColor: '#eee', height: '20px', width: '60%' }}></div>
+    </div>
+  );
 
   return (
     <div>
       <h1>Data from API</h1>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
+      { isLoading ? <Skeleton /> : <pre>{JSON.stringify(data, null, 2)}</pre> }
     </div>
   );
 };
