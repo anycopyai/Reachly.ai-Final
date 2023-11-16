da2196a3fcf893a8050a2a62f762efc0f8172a17
rdmi-stack <76124485+rdmi-stack@users.noreply.github.com>
2023-11-16 16:25:13 +0000

    fdfdfdf

--- components/web/Hero.js
+++ components/web/Hero.js
@@ -4,39 +4,39 @@ import { IoShieldCheckmarkOutline } from 'react-icons/io5';
 
 const Hero = () => {
   return (
-    <section className="bg-[#1d0254] text-white">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="hero flex flex-wrap justify-between items-start py-24 md:py-32">
-          <div className="md:w-1/2 mb-10 md:mb-0 space-y-4">
-            <h1 className="text-5xl md:text-6xl xl:text-7xl font-bold leading-none mb-4" style={{ fontFamily: "'Inter', sans-serif" }}>
-              Personalized Email Outreach At Scale with AI
+    <section className="bg-white text-gray-800">
+      <div className="container mx-auto px-4 py-16">
+        <div className="flex flex-wrap items-center">
+          <div className="w-full lg:w-1/2">
+            <h1 className="text-5xl lg:text-6xl font-bold mb-8 leading-tight">
+              <span className="text-blue-600">Elevate</span> Your <br />
+              <span className="text-blue-600">Email Outreach</span> with AI
             </h1>
-            <p className="text-sm md:text-base leading-snug" style={{ fontFamily: "'Inter', sans-serif" }}>
-              Elevate your email campaigns with AI-driven personalization. Secure more meetings, amplify replies, and boost revenue. Simple, effective, and scalable.
+            <p className="text-lg mb-10 font-light leading-relaxed">
+              Transform your email campaigns with AI-driven personalization. Achieve higher engagement, increase replies, and amplify your revenue. Start a new journey in email marketing.
             </p>
-            <div className="flex flex-wrap items-center gap-4 mt-4">
+            <div className="flex items-center gap-6 mb-8">
               <Link href="/SignUp">
-                <a className="inline-block bg-blue-600 text-base font-medium px-6 py-3 rounded-md hover:bg-blue-700 transition duration-300" style={{ fontFamily: "'Inter', sans-serif" }}>
-                  Try Free for 14 Days
+                <a className="inline-flex items-center justify-center bg-blue-600 hover:bg-blue-700 text-white font-medium rounded-md text-lg px-8 py-4">
+                  Start Your Free Trial
                 </a>
               </Link>
-              <p className="text-xs font-light italic" style={{ fontFamily: "'Inter', sans-serif", alignSelf: 'center' }}>
-                No Credit Card Required
-              </p>
+              <span className="text-sm italic">
+                No Credit Card Needed
+              </span>
             </div>
-            <div className="flex items-center mt-4">
-              <IoShieldCheckmarkOutline className="text-blue-300 text-2xl mr-2" />
-              <span className="text-xs" style={{ fontFamily: "'Inter', sans-serif" }}>GDPR Compliant</span>
-              <FaRegThumbsUp className="text-blue-300 text-2xl ml-6 mr-2" />
-              <span className="text-xs" style={{ fontFamily: "'Inter', sans-serif" }}>5 Star Rated on Trustpilot</span>
+            <div className="flex items-center text-sm">
+              <IoShieldCheckmarkOutline className="text-blue-500 text-3xl mr-4" />
+              <span className="mr-8">GDPR Compliant</span>
+              <FaRegThumbsUp className="text-blue-500 text-3xl mr-4" />
+              <span>Highly Rated on Trustpilot</span>
             </div>
           </div>
-          <div className="md:w-1/2 flex justify-center">
+          <div className="w-full lg:w-1/2 flex justify-center mt-10 lg:mt-0">
             <img
-              className="w-full max-w-lg mx-auto md:ml-auto"
+              className="w-full max-w-lg"
               src="https://via.placeholder.com/400"
               alt="Illustration of engagement"
-              style={{ maxWidth: '500px' }}
             />
           </div>
         </div>
--- pages/SignUp.js
+++ pages/SignUp.js
@@ -1,10 +1,9 @@
 import React, { useState } from 'react';
-import { createUserWithEmailAndPassword, signInWithPopup, GoogleAuthProvider, OAuthProvider } from 'firebase/auth';
+import axios from 'axios';
 import { useRouter } from 'next/router';
-import { auth } from '../lib/firebase'; // Adjust the path
-import { FcGoogle } from 'react-icons/fc'; // Ensure you have react-icons installed
-import { BsMicrosoft } from 'react-icons/bs'; // For Microsoft icon
-import { AiOutlineLoading3Quarters } from 'react-icons/ai'; // For loading icon
+import { FcGoogle } from 'react-icons/fc';
+import { BsMicrosoft } from 'react-icons/bs';
+import { AiOutlineLoading3Quarters } from 'react-icons/ai';
 
 export default function Signup() {
     const router = useRouter();
@@ -17,8 +16,6 @@ export default function Signup() {
     });
     const [errors, setErrors] = useState({});
     const [isSubmitting, setIsSubmitting] = useState(false);
-  const [isGoogleSigningUp, setIsGoogleSigningUp] = useState(false);
-  const [isMicrosoftSigningUp, setIsMicrosoftSigningUp] = useState(false);
 
     const handleChange = (e) => {
         const { name, value, checked, type } = e.target;
@@ -49,132 +46,105 @@ export default function Signup() {
             return;
         }
         try {
-            await createUserWithEmailAndPassword(auth, userData.email, userData.password);
-            router.push('/dashboard');
+            await axios.post('https://api.elixcent.com/signup', {
+                name: userData.name,
+                email: userData.email,
+                password: userData.password
+            });
+            router.push('/dashboard'); // Replace with your dashboard path
         } catch (error) {
             setIsSubmitting(false);
-            switch (error.code) {
-                case 'auth/email-already-in-use':
-                    setErrors({ form: "Email already in use. Please login to continue." });
-                    break;
-                // Add other cases as needed
-                default:
-                    setErrors({ form: "An unexpected error occurred. Please try again." });
-            }
+            setErrors({ form: error.response.data.error || "An unexpected error occurred. Please try again." });
         }
     };
 
-    const handleExternalSignup = async (provider) => {
-        setIsSubmitting(true);
-        try {
-            await signInWithPopup(auth, provider);
-            router.push('/dashboard');
-        } catch (error) {
-            setIsSubmitting(false);
-            // Handle errors for external providers here
-            setErrors({ form: "An error occurred with the external provider. Please try again." });
-        }
-    };
+// ... [Previous code, including imports and function definitions]
 
-  const handleGoogleSignup = async () => {
-      setIsGoogleSigningUp(true);
-      const provider = new GoogleAuthProvider();
-      try {
-          await signInWithPopup(auth, provider);
-          router.push('/dashboard');
-      } catch (error) {
-          setIsGoogleSigningUp(false);
-          // Handle errors for Google sign-in here
-          setErrors({ form: "An error occurred with Google sign-in. Please try again." });
-      }
-  };
+const handleGoogleSignup = async () => {
+    // Implement Google signup logic here
+    // Example: signInWithPopup(auth, new GoogleAuthProvider());
+    // ...
+};
 
+const handleMicrosoftSignup = async () => {
+    // Implement Microsoft signup logic here
+    // Example: signInWithPopup(auth, new OAuthProvider('microsoft.com'));
+    // ...
+};
 
-  const handleMicrosoftSignup = async () => {
-      setIsMicrosoftSigningUp(true);
-      const provider = new OAuthProvider('microsoft.com');
-      try {
-          await signInWithPopup(auth, provider);
-          router.push('/dashboard');
-      } catch (error) {
-          setIsMicrosoftSigningUp(false);
-          // Handle errors for Microsoft sign-in here
-          setErrors({ form: "An error occurred with Microsoft sign-in. Please try again." });
-      }
-  };
-
-
-    return (
-        <div className="flex flex-col md:flex-row min-h-screen">
-            {/* Signup Form */}
-            <div className="w-full md:w-1/2 flex flex-col justify-center items-center bg-white px-6 py-10 md:p-16">
-                <h2 className="text-3xl font-bold text-gray-800 mb-2">Start Your Free Trial</h2>
-                <p className="mb-4 text-gray-600">Join Elixcent today and explore new possibilities!</p>
-                {errors.form && <p className="text-red-500 mb-4">{errors.form}</p>}
-                <form onSubmit={handleSignup} className="w-full max-w-md space-y-4">
-                    {['name', 'email', 'password', 'confirmPassword'].map(field => (
-                        <div key={field}>
-                            <input
-                                type={field === 'confirmPassword' ? 'password' : (field.includes('password') ? 'password' : 'text')}
-                                id={field}
-                                name={field}
-                                placeholder={field.replace(/([A-Z])/g, ' $1').trim()}
-                                value={userData[field]}
-                                onChange={handleChange}
-                                disabled={isSubmitting}
-                                className={`w-full h-12 px-4 rounded-md shadow-sm focus:ring focus:ring-indigo-200 focus:ring-opacity-50 ${errors[field] ? 'ring-2 ring-red-500' : 'ring-1 ring-gray-300'}`}
-                            />
-                            {errors[field] && <p className="text-red-500 text-xs mt-1">{errors[field]}</p>}
-                        </div>
-                    ))}
-                    <div className="flex items-center mt-4">
+return (
+    <div className="flex flex-col md:flex-row min-h-screen">
+        {/* Signup Form */}
+        <div className="w-full md:w-1/2 flex flex-col justify-center items-center bg-white px-6 py-10 md:p-16">
+            <h2 className="text-3xl font-bold text-gray-800 mb-2">Start Your Free Trial</h2>
+            <p className="mb-4 text-gray-600">Join Elixcent today and explore new possibilities!</p>
+            {errors.form && <p className="text-red-500 mb-4">{errors.form}</p>}
+            <form onSubmit={handleSignup} className="w-full max-w-md space-y-4">
+                {/* Form Fields */}
+                {/* ... */}
+                {['name', 'email', 'password', 'confirmPassword'].map(field => (
+                    <div key={field}>
                         <input
-                            type="checkbox"
-                            id="consent"
-                            name="consent"
-                            checked={userData.consent}
+                            type={field === 'confirmPassword' ? 'password' : (field.includes('password') ? 'password' : 'text')}
+                            id={field}
+                            name={field}
+                            placeholder={field.replace(/([A-Z])/g, ' $1').trim()}
+                            value={userData[field]}
                             onChange={handleChange}
                             disabled={isSubmitting}
-                            className="checkbox checkbox-primary mr-2"
+                            className={`w-full h-12 px-4 rounded-md shadow-sm focus:ring focus:ring-indigo-200 focus:ring-opacity-50 ${errors[field] ? 'ring-2 ring-red-500' : 'ring-1 ring-gray-300'}`}
                         />
-                        <label htmlFor="consent" className="text-sm text-gray-600">
-                            By signing up, I agree to Elixcent's Terms of Service and Privacy Policy.
-                        </label>
+                        {errors[field] && <p className="text-red-500 text-xs mt-1">{errors[field]}</p>}
                     </div>
-                    {errors.consent && <p className="text-red-500 text-xs mt-1">{errors.consent}</p>}
-                    <button 
-                        type="submit" 
-                        disabled={isSubmitting} 
-                        className="w-full mt-6 px-4 py-2 text-white bg-indigo-600 rounded-md hover:bg-indigo-700 focus:ring-2 focus:ring-indigo-500 focus:ring-opacity-50 transition duration-300 ease-in-out flex items-center justify-center"
-                    >
-                      {isSubmitting ? <><AiOutlineLoading3Quarters className="animate-spin mr-2" /> Creating Your Account</> : 'Create Account'}
-
-                    </button>
-                  <button 
-                      type="button" 
-                      onClick={handleGoogleSignup} 
-                      disabled={isGoogleSigningUp} 
-                      className="w-full px-4 py-2 border rounded-md hover:bg-gray-50 transition duration-300 ease-in-out mb-2 flex items-center justify-center"
-                  >
-                      {isGoogleSigningUp ? <><AiOutlineLoading3Quarters className="animate-spin mr-2" /> Signing Up with Google</> : <><FcGoogle className="text-xl mr-2" /> Sign Up with Google</>}
-                  </button>
-
-                  <button 
-                      type="button" 
-                      onClick={handleMicrosoftSignup} 
-                      disabled={isMicrosoftSigningUp} 
-                      className="w-full px-4 py-2 border rounded-md hover:bg-gray-50 transition duration-300 ease-in-out flex items-center justify-center"
-                  >
-                      {isMicrosoftSigningUp ? <><AiOutlineLoading3Quarters className="animate-spin mr-2" /> Signing Up with Microsoft</> : <><BsMicrosoft className="text-xl mr-2" /> Sign Up with Microsoft</>}
-                  </button>
-
-                </form>
-            </div>
-
-            {/* Full Height Image */}
-            <div className="w-full h-64 md:h-auto md:w-1/2 bg-cover bg-no-repeat bg-center" style={{ backgroundImage: 'url("https://static.getprospect.com/img/help-center/outreach.jpg")' }}>
-                {/* Optional: Overlay or additional content here */}
-            </div>
+                ))}
+                <div className="flex items-center mt-4">
+                    <input
+                        type="checkbox"
+                        id="consent"
+                        name="consent"
+                        checked={userData.consent}
+                        onChange={handleChange}
+                        disabled={isSubmitting}
+                        className="checkbox checkbox-primary mr-2"
+                    />
+                    <label htmlFor="consent" className="text-sm text-gray-600">
+                        By signing up, I agree to Elixcent's Terms of Service and Privacy Policy.
+                    </label>
+                </div>
+                {errors.consent && <p className="text-red-500 text-xs mt-1">{errors.consent}</p>}
+                <button 
+                    type="submit" 
+                    disabled={isSubmitting} 
+                    className="w-full mt-6 px-4 py-2 text-white bg-indigo-600 rounded-md hover:bg-indigo-700 focus:ring-2 focus:ring-indigo-500 focus:ring-opacity-50 transition duration-300 ease-in-out flex items-center justify-center"
+                >
+                  {isSubmitting ? <><AiOutlineLoading3Quarters className="animate-spin mr-2" /> Creating Your Account</> : 'Create Account'}
+                </button>
+                <button 
+                    type="button" 
+                    onClick={handleGoogleSignup} 
+                    disabled={isSubmitting} 
+                    className="w-full px-4 py-2 border rounded-md hover:bg-gray-50 transition duration-300 ease-in-out mb-2 flex items-center justify-center"
+                >
+                    <FcGoogle className="text-xl mr-2" /> Sign Up with Google
+                </button>
+                <button 
+                    type="button" 
+                    onClick={handleMicrosoftSignup} 
+                    disabled={isSubmitting} 
+                    className="w-full px-4 py-2 border rounded-md hover:bg-gray-50 transition duration-300 ease-in-out flex items-center justify-center"
+                >
+                    <BsMicrosoft className="text-xl mr-2" /> Sign Up with Microsoft
+                </button>
+            </form>
+            <p className="mt-4 text-sm">
+                Already have an account? <a href="/Login" className="text-indigo-600 hover:text-indigo-500">Login here</a>
+            </p>
         </div>
-    );
+
+        {/* Full Height Image */}
+        <div className="w-full h-64 md:h-auto md:w-1/2 bg-cover bg-no-repeat bg-center" style={{ backgroundImage: 'url("https://static.getprospect.com/img/help-center/outreach.jpg")' }}>
+            {/* Optional: Overlay or additional content here */}
+        </div>
+    </div>
+);
 }
