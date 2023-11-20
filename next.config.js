c820f6d521cd1b1fe0d3cb8003cf8ffe88b1c6b8
rdmi-stack <76124485+rdmi-stack@users.noreply.github.com>
2023-11-20 20:16:03 +0000

    dfdf

--- next.config.js
+++ next.config.js
@@ -1,40 +1,47 @@
-module.exports = {
-  reactStrictMode: true,
-}
-
-
-// Injected content via Sentry wizard below
-
 const { withSentryConfig } = require("@sentry/nextjs");
 
-module.exports = withSentryConfig(
-  module.exports,
-  {
-    // For all available options, see:
-    // https://github.com/getsentry/sentry-webpack-plugin#options
+const moduleExports = {
+  reactStrictMode: true,
 
-    // Suppresses source map uploading logs during build
-    silent: true,
-    org: "rdmi",
-    project: "javascript-nextjs",
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/Login',
+        permanent: true,
+      },
+    ];
   },
-  {
-    // For all available options, see:
-    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+};
 
-    // Upload a larger set of source maps for prettier stack traces (increases build time)
-    widenClientFileUpload: true,
+const sentryWebpackPluginOptions = {
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options
 
-    // Transpiles SDK to be compatible with IE11 (increases bundle size)
-    transpileClientSDK: true,
+  // Suppresses source map uploading logs during build
+  silent: true,
+  org: "rdmi",
+  project: "javascript-nextjs",
+};
 
-    // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
-    tunnelRoute: "/monitoring",
+const sentryNextJsOptions = {
+  // For all available options, see:
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
-    // Hides source maps from generated client bundles
-    hideSourceMaps: true,
+  // Upload a larger set of source maps for prettier stack traces (increases build time)
+  widenClientFileUpload: true,
 
-    // Automatically tree-shake Sentry logger statements to reduce bundle size
-    disableLogger: true,
-  }
-);
+  // Transpiles SDK to be compatible with IE11 (increases bundle size)
+  transpileClientSDK: true,
+
+  // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
+  tunnelRoute: "/monitoring",
+
+  // Hides source maps from generated client bundles
+  hideSourceMaps: true,
+
+  // Automatically tree-shake Sentry logger statements to reduce bundle size
+  disableLogger: true,
+};
+
+module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions, sentryNextJsOptions);
--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,9 @@ import Navbar from '../components/web/Navbar';
 import Hero from '../components/web/Hero';
 import Features from '../components/web/Features'; // This line imports the Features component
 import Footer from '../components/web/Footer'; // Make sure to import the Footer component
+import withAuth from '../hoc/withAuth'; // Adjust the import path as needed
 
-export default function Home() {
+function Home() {
   return (
     <div>
       <Navbar />
@@ -15,3 +16,6 @@ export default function Home() {
     </div>
   );
 }
+
+// Adjust the import path as needed
+export default withAuth(Home);
