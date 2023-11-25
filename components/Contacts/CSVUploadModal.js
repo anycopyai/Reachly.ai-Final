c17db8ad02d89dee4a83ec4dc5a3dfd4c7b894d6
Ranjit9119 <info@rdmi.in>
2023-11-25 02:12:00 +0000

    jjjj

--- components/Contacts/CSVUploadModal.js
+++ components/Contacts/CSVUploadModal.js
@@ -1,73 +1,22 @@
-import React, { useState } from 'react';
-import { useDropzone } from 'react-dropzone';
-import Papa from 'papaparse';
-import axios from 'axios';
-import { FiUpload } from 'react-icons/fi'; // Importing an upload icon
-import { CircularProgressbar } from 'react-circular-progressbar'; // For progress bar
-import 'react-circular-progressbar/dist/styles.css'; // Import progress bar styles
+import React from 'react';
 
-function CSVUploadModal({ isOpen, onClose }) {
-    const [isFileOver, setIsFileOver] = useState(false);
-    const [uploadProgress, setUploadProgress] = useState(0);
-    const [isUploading, setIsUploading] = useState(false);
-
-    const onDrop = acceptedFiles => {
-        const file = acceptedFiles[0];
-        setIsUploading(true);
-        Papa.parse(file, {
-            complete: (result) => {
-                // Example: upload to the backend
-                const config = {
-                    onUploadProgress: progressEvent => {
-                        let percentCompleted = Math.round((progressEvent.loaded * 100) / progressEvent.total);
-                        setUploadProgress(percentCompleted);
-                    }
-                };
-                axios.post('/api/upload-csv', { data: result.data }, config)
-                    .then(response => {
-                        // Handle response
-                        setIsUploading(false);
-                        onClose(); // Close modal after upload
-                    })
-                    .catch(error => {
-                        console.error('Error uploading CSV data:', error);
-                        setIsUploading(false);
-                    });
-            },
-            header: true
-        });
-    };
-
-    const { getRootProps, getInputProps } = useDropzone({
-        onDrop,
-        onDragEnter: () => setIsFileOver(true),
-        onDragLeave: () => setIsFileOver(false)
-    });
-
-    if (!isOpen) {
-        return null;
+const CSVUploadModal = ({ isOpen, onClose, onUpload }) => {
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      onUpload(file);
     }
+  };
 
-    return (
-        <dialog id="csv_upload_modal" className="modal modal-bottom sm:modal-middle" open={isOpen}>
-            <div className="modal-box">
-                <div {...getRootProps()} className="dropzone py-4 flex flex-col items-center justify-center">
-                    <input {...getInputProps()} />
-                    {isUploading ? (
-                        <CircularProgressbar value={uploadProgress} text={`${uploadProgress}%`} />
-                    ) : (
-                        <>
-                            <FiUpload size={50} color={isFileOver ? 'green' : 'black'} />
-                            <p>{isFileOver ? 'Drop the files here ...' : 'Drag \'n\' drop CSV file here, or click to select file'}</p>
-                        </>
-                    )}
-                </div>
-                <div className="modal-action">
-                    <button className="btn" onClick={onClose} disabled={isUploading}>Close</button>
-                </div>
-            </div>
-        </dialog>
-    );
-}
+  if (!isOpen) return null;
+
+  return (
+    <div>
+      {/* Modal content */}
+      <input type="file" accept=".csv" onChange={handleFileChange} />
+      <button onClick={onClose}>Close</button>
+    </div>
+  );
+};
 
 export default CSVUploadModal;
--- pages/Contacts.js
+++ pages/Contacts.js
@@ -1,55 +1,137 @@
 import React, { useState, useCallback } from 'react';
-import { useDropzone } from 'react-dropzone';
 import Papa from 'papaparse';
-import axios from 'axios';
-
 import Sidebar from '../components/dashboard/Sidebar';
 import DaisyUIMenu from '../components/dashboard/DaisyUIMenu';
 import ContactTable from '../components/Contacts/ContactTable';
-import CSVUploadModal from '../components/Contacts/CSVUploadModal'; // Adjust the import path as needed
+import CSVUploadModal from '../components/Contacts/CSVUploadModal';
 import withAuth from '../hoc/withAuth';
+import { AiOutlineEdit, AiOutlineDelete } from 'react-icons/ai'; // Import the icons
+import AddContactModal from '../components/Contacts/AddContactModal'; // Adjust path as necessary
+import { AiOutlineFileExcel } from 'react-icons/ai';
+
+
 
 function Contacts() {
     const [contacts, setContacts] = useState([]);
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const [isTableVisible, setIsTableVisible] = useState(false);
+    const [selectedRows, setSelectedRows] = useState(new Set());
+    const [isCSVModalOpen, setIsCSVModalOpen] = useState(false);
+    const [dropdownOpen, setDropdownOpen] = useState(false);
+    const [isAddModalOpen, setIsAddModalOpen] = useState(false);
 
-    // CSV Upload handler
-    const onDrop = useCallback(acceptedFiles => {
-        const file = acceptedFiles[0];
-        Papa.parse(file, {
-            complete: (result) => {
-                console.log('Parsed CSV Data:', result.data);
-                // Send data to backend for processing
-                axios.post('/api/upload-csv', { data: result.data })
-                    .then(response => {
-                        // Handle the response here, like updating contacts state
-                    })
-                    .catch(error => {
-                        // Handle errors
-                        console.error('Error uploading CSV data:', error);
-                    });
+    const openAddModal = () => setIsAddModalOpen(true);
+    const closeAddModal = () => setIsAddModalOpen(false);
+
+    const addNewContact = (contact) => {
+        setContacts([...contacts, { ...contact, id: contacts.length + 1 }]);
+    };
+
+    const handleSelectRow = (id) => {
+        const newSelectedRows = new Set(selectedRows);
+        newSelectedRows.has(id) ? newSelectedRows.delete(id) : newSelectedRows.add(id);
+        setSelectedRows(newSelectedRows);
+    };
+
+    const handleSelectAll = () => {
+        if (selectedRows.size === contacts.length) {
+            setSelectedRows(new Set());
+        } else {
+            setSelectedRows(new Set(contacts.map(contact => contact.id)));
+        }
+    };
+    const handleCSVUploadClick = () => {
+        // Logic to handle CSV upload
+    };
+    const handleCSVUpload = useCallback(acceptedFiles => {
+        Papa.parse(acceptedFiles[0], {
+            header: true,
+            complete: (results) => {
+                setContacts([...contacts, ...results.data]);
+                setIsCSVModalOpen(false);
             },
-            header: true
+            error: (error) => {
+                console.error('Error parsing CSV:', error);
+            }
         });
-    }, []);
+    }, [contacts]);
+
+    const openCSVModal = () => {
+        setIsCSVModalOpen(true);
+        setDropdownOpen(false);
+    };
+
+    const closeCSVModal = () => setIsCSVModalOpen(false);
 
     return (
         <div className="flex h-screen bg-gray-100">
             <Sidebar />
-            <div className="flex flex-col flex-1">
+            <div className="flex flex-col flex-1 relative">
                 <DaisyUIMenu />
-                <div className="flex-1 p-10">
-                    <button className="btn" onClick={() => setIsModalOpen(true)}>Add Contacts</button>
 
-                    <CSVUploadModal
-                        isOpen={isModalOpen}
-                        onClose={() => setIsModalOpen(false)}
-                        onDrop={onDrop}
-                    />
-
-                    {isTableVisible && <ContactTable contacts={contacts} />}
+                {/* Table and Add Contact Button */}
+                <div className="p-4">
+                <button className="btn btn-primary mb-4" onClick={openAddModal}>Add Contact</button>
+                <button className="btn btn-outline btn-primary" onClick={handleCSVUploadClick}>
+                    <AiOutlineFileExcel className="text-lg mr-2" />
+                    Upload CSV
+                </button>
+{/* Add Contact Modal */}
+<AddContactModal 
+    isOpen={isAddModalOpen} 
+    onClose={closeAddModal} 
+    onAddContact={addNewContact} 
+/>
+                    <div className="overflow-x-auto">
+                        <table className="table w-full">
+                            {/* Table header */}
+                            <thead>
+                                <tr>
+                                    <th>
+                                        <input
+                                            type="checkbox"
+                                            className="checkbox"
+                                            checked={selectedRows.size === contacts.length}
+                                            onChange={handleSelectAll}
+                                        />
+                                    </th>
+                                    <th>Name</th>
+                                    {/* Add more headers as needed */}
+                                    <th>Actions</th>
+                                </tr>
+                            </thead>
+                            {/* Table body */}
+                            <tbody>
+                                {contacts.map(contact => (
+                                    <tr key={contact.id} className={selectedRows.has(contact.id) ? 'bg-blue-200' : ''}>
+                                        <td>
+                                            <input
+                                                type="checkbox"
+                                                className="checkbox"
+                                                checked={selectedRows.has(contact.id)}
+                                                onChange={() => handleSelectRow(contact.id)}
+                                            />
+                                        </td>
+                                        <td>{contact.name}</td>
+                                        {/* Add more cells as needed */}
+                                        <td>
+                                            <button className="btn btn-ghost btn-xs">
+                                                <AiOutlineEdit />
+                                            </button>
+                                            <button className="btn btn-ghost btn-xs">
+                                                <AiOutlineDelete />
+                                            </button>
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
+
+                {/* Button to Open CSV Modal */}
+                <button onClick={openCSVModal} className="contact-button">Upload CSV</button>
+
+                {/* CSV Upload Modal */}
+                <CSVUploadModal isOpen={isCSVModalOpen} onClose={closeCSVModal} onDrop={handleCSVUpload} />
             </div>
         </div>
     );
