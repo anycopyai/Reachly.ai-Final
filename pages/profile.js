f98c4388fc4de3e41868e43d5bbc49fc95f64ba3
Ranjit9119 <info@rdmi.in>
2023-11-27 22:45:00 +0530

    dd

--- pages/profile.js
+++ pages/profile.js
@@ -1,58 +1,68 @@
-import { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { MdDashboard, MdContacts, MdList, MdMenu, MdClose, MdPerson, MdSettings, MdSubscriptions, MdVpnKey, MdApi } from 'react-icons/md';
+import Link from 'next/link';
 import { useRouter } from 'next/router';
+import { Formik, Form, Field } from 'formik';
 
 const ProfileSettings = () => {
-  const router = useRouter();
+  const [isSidebarCollapsed, setIsSidebarCollapsed] = useState(false);
   const [activeTab, setActiveTab] = useState('profile');
+  const router = useRouter();
 
   const handleTabClick = (tab) => {
     setActiveTab(tab);
-    router.push(`/settings/${tab}`);
   };
 
-  return (
-    <div className="container mx-auto p-4">
-      <div className="border-b border-gray-300">
-        <nav className="flex flex-wrap -mb-px text-sm">
-          <button onClick={() => handleTabClick('profile')} 
-            className={`mr-5 pb-3 ${activeTab === 'profile' ? 'border-b-2 border-blue-500 text-blue-600' : 'text-gray-500'}`}>
-            Profile
-          </button>
-          <button onClick={() => handleTabClick('integration')} 
-            className={`mr-5 pb-3 ${activeTab === 'integration' ? 'border-b-2 border-blue-500 text-blue-600' : 'text-gray-500'}`}>
-            Integration
-          </button>
-          <button onClick={() => handleTabClick('subscription')} 
-            className={`mr-5 pb-3 ${activeTab === 'subscription' ? 'border-b-2 border-blue-500 text-blue-600' : 'text-gray-500'}`}>
-            Subscription
-          </button>
-          <button onClick={() => handleTabClick('api')} 
-            className={`mr-5 pb-3 ${activeTab === 'api' ? 'border-b-2 border-blue-500 text-blue-600' : 'text-gray-500'}`}>
-            API
-          </button>
-          <button onClick={() => handleTabClick('security')} 
-            className={`mr-5 pb-3 ${activeTab === 'security' ? 'border-b-2 border-blue-500 text-blue-600' : 'text-gray-500'}`}>
-            Security
-          </button>
-        </nav>
+  // Sidebar
+  const Sidebar = ({ isCollapsed }) => {
+    const menuItems = [
+      { icon: <MdPerson />, label: 'Profile', href: '#profile' },
+      { icon: <MdSettings />, label: 'Integration', href: '#integration' },
+      { icon: <MdSubscriptions />, label: 'Subscription', href: '#subscription' },
+      { icon: <MdApi />, label: 'API', href: '#api' },
+      { icon: <MdVpnKey />, label: 'Security', href: '#security' }
+    ];
+
+    const renderMenuItem = (item) => (
+      <li key={item.label} className="mb-4">
+        <a href={item.href} onClick={() => handleTabClick(item.label.toLowerCase())} className={`flex items-center space-x-4 p-3 rounded-md hover:bg-gray-200 ${activeTab === item.label.toLowerCase() ? 'bg-gray-300' : ''}`}>
+          {React.cloneElement(item.icon, { size: 24, className: 'text-blue-500' })}
+          <span className={`${isCollapsed ? 'hidden' : 'block'}`}>{item.label}</span>
+        </a>
+      </li>
+    );
+
+    return (
+      <div className={`fixed inset-y-0 left-0 z-40 w-64 px-2 py-4 bg-white text-gray-700 border-r`}>
+        <ul className="menu w-full">
+          {menuItems.map(renderMenuItem)}
+        </ul>
       </div>
-      
-      <div className="mt-4">
-        {activeTab === 'profile' && <ProfileComponent />}
-        {activeTab === 'integration' && <IntegrationComponent />}
-        {activeTab === 'subscription' && <SubscriptionComponent />}
-        {activeTab === 'api' && <APIComponent />}
-        {activeTab === 'security' && <SecurityComponent />}
+    );
+  };
+
+  // Main Content
+  return (
+    <div className="flex h-screen">
+      <Sidebar isCollapsed={isSidebarCollapsed} />
+      <div className="flex-1 bg-gray-100 p-6">
+        <div>
+          {activeTab === 'profile' && (
+            <Formik initialValues={{ name: '' }} onSubmit={(values) => console.log(values)}>
+              <Form>
+                <Field name="name" type="text" placeholder="Name" className="p-2 border rounded" />
+                <button type="submit" className="p-2 bg-blue-500 text-white rounded ml-2">Save</button>
+              </Form>
+            </Formik>
+          )}
+          {activeTab === 'integration' && <div>Integration Settings</div>}
+          {activeTab === 'subscription' && <div>Subscription Settings</div>}
+          {activeTab === 'api' && <div>API Settings</div>}
+          {activeTab === 'security' && <div>Security Settings</div>}
+        </div>
       </div>
     </div>
   );
 };
-
-// Placeholder components for each tab's content
-const ProfileComponent = () => <div>Profile Content</div>;
-const IntegrationComponent = () => <div>Integration Content</div>;
-const SubscriptionComponent = () => <div>Subscription Content</div>;
-const APIComponent = () => <div>API Content</div>;
-const SecurityComponent = () => <div>Security Content</div>;
 
 export default ProfileSettings;
