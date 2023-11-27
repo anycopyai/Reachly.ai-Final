008ea390864edcf2206e4bffa7cebaf842be76f3
Ranjit9119 <info@rdmi.in>
2023-11-28 03:55:56 +0530

    jkj

--- pages/index.js
+++ pages/index.js
@@ -7,9 +7,95 @@ import Footer from '../components/web/Footer';       // Footer component
 export default function Home() {
   return (
     <div>
+      {/* Top Bar */}
+      <div className="bg-indigo-600 px-4 py-3 text-white sm:flex sm:items-center sm:justify-between sm:px-6 lg:px-8">
+        <p className="text-center font-medium sm:text-left">
+          Love Alpine JS?
+          <br className="sm:hidden" />
+          Check out this new deep dive course!
+        </p>
+        <a
+          className="mt-4 block rounded-lg bg-white px-5 py-3 text-center text-sm font-medium text-indigo-600 transition hover:bg-white/90 focus:outline-none focus:ring active:text-indigo-500 sm:mt-0"
+          href="#"
+        >
+          Learn More
+        </a>
+      </div>
       <Navbar />
       <Hero />
-      <feature1 /> {/* Correctly include the FeaturesSection component */}
+      <FeaturesSection />
+
+      {/* Container for Collapsible Sections */}
+      <div className="container mx-auto px-4 py-8">
+        <div className="grid grid-cols-1 gap-4">
+        <div className="space-y-4">
+  <details className="group [&_summary::-webkit-details-marker]:hidden" open>
+    <summary
+      className="flex cursor-pointer items-center justify-between gap-1.5 rounded-lg bg-gray-50 p-4 text-gray-900"
+    >
+      <h2 className="font-medium">
+        Lorem ipsum dolor sit amet consectetur adipisicing?
+      </h2>
+
+      <svg
+        className="h-5 w-5 shrink-0 transition duration-300 group-open:-rotate-180"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          d="M19 9l-7 7-7-7"
+        />
+      </svg>
+    </summary>
+
+    <p className="mt-4 px-4 leading-relaxed text-gray-700">
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic veritatis
+      molestias culpa in, recusandae laboriosam neque aliquid libero nesciunt
+      voluptate dicta quo officiis explicabo consequuntur distinctio corporis
+      earum similique!
+    </p>
+  </details>
+
+  <details className="group [&_summary::-webkit-details-marker]:hidden">
+    <summary
+      className="flex cursor-pointer items-center justify-between gap-1.5 rounded-lg bg-gray-50 p-4 text-gray-900"
+    >
+      <h2 className="font-medium">
+        Lorem ipsum dolor sit amet consectetur adipisicing?
+      </h2>
+
+      <svg
+        className="h-5 w-5 shrink-0 transition duration-300 group-open:-rotate-180"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          d="M19 9l-7 7-7-7"
+        />
+      </svg>
+    </summary>
+
+    <p className="mt-4 px-4 leading-relaxed text-gray-700">
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic veritatis
+      molestias culpa in, recusandae laboriosam neque aliquid libero nesciunt
+      voluptate dicta quo officiis explicabo consequuntur distinctio corporis
+      earum similique!
+    </p>
+  </details>
+</div>
+        </div>
+      </div>
+
       <Footer />
     </div>
   );
