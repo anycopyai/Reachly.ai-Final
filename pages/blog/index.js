8a6417a5f0a98166030473f98d2e39b11bfe29d6
Ranjit9119 <info@rdmi.in>
2023-11-29 18:20:13 +0530

    ss

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,8 +1,285 @@
+import React, { useState } from 'react';
+import Footer from '../../components/web/Footer';       // Footer component
+const Modal = () => {
   const [isOpen, setIsOpen] = useState(false);
-    return (
-        <div>
-            <BlogLayout />
-            {/* Other content for the page */}
+
+  const toggleModal = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+
+    
+    <>
+      <body class="bg-slate-900 flex h-full">
+
+    <div className="max-w-[50rem] flex flex-col mx-auto w-full h-full">
+  {/* ========== HEADER ========== */}
+  <header className="mb-auto flex flex-wrap sm:justify-start sm:flex-nowrap z-50 w-full text-sm py-4">
+    <nav
+      className="w-full px-4 sm:flex sm:items-center sm:justify-between sm:px-6 lg:px-8"
+      aria-label="Global"
+    >
+      <div className="flex items-center justify-between">
+        <a
+          className="flex-none text-xl font-semibold text-white focus:outline-none focus:ring-1 focus:ring-gray-600"
+          href="#"
+          aria-label="Brand"
+        >
+          Elixcent
+        </a>
+        <div className="sm:hidden">
+          <button
+            type="button"
+            className="hs-collapse-toggle p-2 inline-flex justify-center items-center gap-2 rounded-lg border border-gray-700 hover:border-gray-600 font-medium text-gray-300 hover:text-white shadow-sm align-middle focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-slate-900 focus:ring-blue-600 transition-all text-sm"
+            data-hs-collapse="#navbar-collapse-with-animation"
+            aria-controls="navbar-collapse-with-animation"
+            aria-label="Toggle navigation"
+          >
+            <svg
+              className="hs-collapse-open:hidden flex-shrink-0 w-4 h-4"
+              xmlns="http://www.w3.org/2000/svg"
+              width={24}
+              height={24}
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth={2}
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <line x1={3} x2={21} y1={6} y2={6} />
+              <line x1={3} x2={21} y1={12} y2={12} />
+              <line x1={3} x2={21} y1={18} y2={18} />
+            </svg>
+            <svg
+              className="hs-collapse-open:block hidden flex-shrink-0 w-4 h-4"
+              xmlns="http://www.w3.org/2000/svg"
+              width={24}
+              height={24}
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth={2}
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M18 6 6 18" />
+              <path d="m6 6 12 12" />
+            </svg>
+          </button>
         </div>
-    );
-}
+      </div>
+      <div
+        id="navbar-collapse-with-animation"
+        className="hs-collapse hidden overflow-hidden transition-all duration-300 basis-full grow sm:block"
+      >
+        <div className="flex flex-col gap-5 mt-5 sm:flex-row sm:items-center sm:justify-end sm:mt-0 sm:ps-5">
+          <a
+            className="font-medium text-white focus:outline-none focus:ring-1 focus:ring-gray-600"
+            href="#"
+            aria-current="page"
+          >
+            Landing
+          </a>
+          <a
+            className="font-medium text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-1 focus:ring-gray-600"
+            href="#"
+          >
+            Account
+          </a>
+          <a
+            className="font-medium text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-1 focus:ring-gray-600"
+            href="#"
+          >
+            Work
+          </a>
+          <a
+            className="font-medium text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-1 focus:ring-gray-600"
+            href="#"
+          >
+            Blog
+          </a>
+        </div>
+      </div>
+    </nav>
+  </header>
+  {/* ========== END HEADER ========== */}
+  {/* ========== MAIN CONTENT ========== */}
+  <main id="content" role="main">
+  <div className="text-center py-10 px-4 sm:px-6 lg:px-8" >
+      <h1 className="block text-2xl font-bold text-white sm:text-4xl">
+Blog      </h1>
+      <p className="mt-3 text-lg text-gray-300">
+        Cover is a one-page template for building simple and beautiful home
+        pages using Tailwind CSS.
+      </p>
+      <div className="mt-5 flex flex-col justify-center items-center gap-2 sm:flex-row sm:gap-3">
+       
+      </div>
+    </div>
+  </main>
+  {/* ========== END MAIN CONTENT ========== */}
+  {/* ========== FOOTER ========== */}
+  <footer className="mt-auto text-center py-5">
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      
+    </div>
+  </footer>
+  {/* ========== END FOOTER ========== */}
+</div></body>
+
+    {/* Card Blog */}
+    <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+      {/* Title */}
+      <div className="max-w-2xl mx-auto text-center mb-10 lg:mb-14">
+        <h2 className="text-2xl font-bold md:text-4xl md:leading-tight dark:text-white">
+          The Blog
+        </h2>
+        <p className="mt-1 text-gray-600 dark:text-gray-400">
+          See how game-changing companies are making the most of every engagement
+          with Preline.
+        </p>
+      </div>
+      {/* End Title */}
+      {/* Grid */}
+      <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        {/* Card */}
+        <a
+          className="group flex flex-col h-full border border-gray-200 hover:border-transparent hover:shadow-lg transition-all duration-300 rounded-xl p-5 dark:border-gray-700 dark:hover:border-transparent dark:hover:shadow-black/[.4] dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+          href="#"
+        >
+          <div className="aspect-w-16 aspect-h-11">
+            <img
+              className="w-full object-cover rounded-xl"
+              src="https://images.unsplash.com/photo-1633114128174-2f8aa49759b0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+              alt="Image Description"
+            />
+          </div>
+          <div className="my-6">
+            <h3 className="text-xl font-semibold text-gray-800 dark:text-gray-300 dark:group-hover:text-white">
+              Announcing a free plan for small teams
+            </h3>
+            <p className="mt-5 text-gray-600 dark:text-gray-400">
+              At Wake, our mission has always been focused on bringing openness.
+            </p>
+          </div>
+          <div className="mt-auto flex items-center gap-x-3">
+            <img
+              className="w-8 h-8 rounded-full"
+              src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=320&h=320&q=80"
+              alt="Image Description"
+            />
+            <div>
+              <h5 className="text-sm text-gray-800 dark:text-gray-200">
+                By Lauren Waller
+              </h5>
+            </div>
+          </div>
+        </a>
+        {/* End Card */}
+        {/* Card */}
+        <a
+          className="group flex flex-col h-full border border-gray-200 hover:border-transparent hover:shadow-lg transition-all duration-300 rounded-xl p-5 dark:border-gray-700 dark:hover:border-transparent dark:hover:shadow-black/[.4] dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+          href="#"
+        >
+          <div className="aspect-w-16 aspect-h-11">
+            <img
+              className="w-full object-cover rounded-xl"
+              src="https://images.unsplash.com/photo-1562851529-c370841f6536?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3540&q=80"
+              alt="Image Description"
+            />
+          </div>
+          <div className="my-6">
+            <h3 className="text-xl font-semibold text-gray-800 dark:text-gray-300 dark:group-hover:text-white">
+              How Google Assistant now helps you record stories for kids
+            </h3>
+            <p className="mt-5 text-gray-600 dark:text-gray-400">
+              Google is constantly updating its consumer AI, Google Assistant,
+              with new features.
+            </p>
+          </div>
+          <div className="mt-auto flex items-center gap-x-3">
+            <img
+              className="w-8 h-8 rounded-full"
+              src="https://images.unsplash.com/photo-1492562080023-ab3db95bfbce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=320&h=320&q=80"
+              alt="Image Description"
+            />
+            <div>
+              <h5 className="text-sm text-gray-800 dark:text-gray-200">
+                By Aaron Larsson
+              </h5>
+            </div>
+          </div>
+        </a>
+        {/* End Card */}
+        {/* Card */}
+        <a
+          className="group flex flex-col h-full border border-gray-200 hover:border-transparent hover:shadow-lg transition-all duration-300 rounded-xl p-5 dark:border-gray-700 dark:hover:border-transparent dark:hover:shadow-black/[.4] dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+          href="#"
+        >
+          <div className="aspect-w-16 aspect-h-11">
+            <img
+              className="w-full object-cover rounded-xl"
+              src="https://images.unsplash.com/photo-1521321205814-9d673c65c167?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3548&q=80"
+              alt="Image Description"
+            />
+          </div>
+          <div className="my-6">
+            <h3 className="text-xl font-semibold text-gray-800 dark:text-gray-300 dark:group-hover:text-white">
+              Front accounts - let's work together
+            </h3>
+            <p className="mt-5 text-gray-600 dark:text-gray-400">
+              Are you an accountant? Are you a company formation advisor?
+            </p>
+          </div>
+          <div className="mt-auto flex items-center gap-x-3">
+            <img
+              className="w-8 h-8 rounded-full"
+              src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=320&h=320&q=80"
+              alt="Image Description"
+            />
+            <div>
+              <h5 className="text-sm text-gray-800 dark:text-gray-200">
+                By Lauren Waller
+              </h5>
+            </div>
+          </div>
+        </a>
+        {/* End Card */}
+      </div>
+      {/* End Grid */}
+      {/* Card */}
+      <div className="mt-12 text-center">
+        <a
+          className="py-3 px-4 inline-flex items-center gap-x-1 text-sm font-medium rounded-full border border-gray-200 bg-white text-blue-600 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-blue-500 dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+          href="#"
+        >
+          Read more
+          <svg
+            className="flex-shrink-0 w-4 h-4"
+            xmlns="http://www.w3.org/2000/svg"
+            width={24}
+            height={24}
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth={2}
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path d="m9 18 6-6-6-6" />
+          </svg>
+        </a>
+      </div>
+      {/* End Card */}
+    </div>
+    {/* End Card Blog */}
+    <Footer />
+
+  </>
+  
+  );
+};
+
+export default Modal;
