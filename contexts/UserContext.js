957aa232fb7d5e8f101b5534217a86b1d5bcbbdf
Ranjit9119 <info@rdmi.in>
2023-11-29 00:01:07 +0000

    dd

--- contexts/UserContext.js
+++ contexts/UserContext.js
@@ -8,7 +8,7 @@ export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
   const [user, setUser] = useState(null);
-  const [loading, setLoading] = useState(true); // Initially true for the first load
+  const [loading, setLoading] = useState(true);
   const [dbLoading, setDbLoading] = useState(false);
   const auth = getAuth(app);
   const router = useRouter();
@@ -16,33 +16,11 @@ export const UserProvider = ({ children }) => {
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (firebaseUser) => {
       setUser(firebaseUser);
-      setLoading(false); // Set to false once auth state is confirmed
+      setLoading(false);
     });
 
     return () => unsubscribe();
   }, [auth]);
-
-  useEffect(() => {
-    const handleStart = () => {
-      // Hide spinner during client-side navigation
-      setLoading(false);
-    };
-
-    const handleComplete = () => {
-      // Reset loading state when navigation is complete
-      setLoading(false);
-    };
-
-    router.events.on('routeChangeStart', handleStart);
-    router.events.on('routeChangeComplete', handleComplete);
-    router.events.on('routeChangeError', handleComplete);
-
-    return () => {
-      router.events.off('routeChangeStart', handleStart);
-      router.events.off('routeChangeComplete', handleComplete);
-      router.events.off('routeChangeError', handleComplete);
-    };
-  }, [router]);
 
   // Logic to determine if the loading spinner should be shown
   const shouldShowLoading = (loading || dbLoading) && router.pathname !== '/';
