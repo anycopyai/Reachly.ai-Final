e9bd3552c027d35dd553dfdd24415a52d758b71d
Ranjit9119 <info@rdmi.in>
2023-11-29 21:41:49 +0530

    d

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 // Import necessary dependencies from 'next' and 'react'
 import Head from 'next/head';
-import styles from '../styles/global.css';
+import styles from '../styles/globals.css';
 
 // This is the main component for your homepage
 export default function Home() {
