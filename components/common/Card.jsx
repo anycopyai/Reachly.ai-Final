35398ddbd0698527d5f30f0d3b3d62a00a0615b6
sagarpandey141 <121431268+sagarpandey141@users.noreply.github.com>
2024-01-25 18:27:15 +0530

    template commit

--- components/common/Card.jsx
+++ components/common/Card.jsx
@@ -3,17 +3,17 @@ import React from 'react'
 const Card = ({icon1,icon2,heading,desc}) => {
    
   return (
-    <div className=' border  w-3/12 h-[13em]'>
+    <div className=' border  w-[25rem]  h-[16em]'>
         {/*icon*/}
         <div className=' flex  justify-between  w-10/12 mx-auto mt-6'>
              {/* Icon 1 */}
-            <div>{icon1 }</div>
+            <div className=' text-green-700'>{icon1 }</div>
 
             {/* Icon 2 */}
-            <div>{icon2}</div>
+            <div className=' text-yellow-600'>{icon2}</div>
         </div>
-        <h1 className=' mt-6 translate-x-6 text-xl font-semibold'>{heading}</h1>
-        <p className=' mt-3 translate-x-6 text-slate-600'>{desc}</p>
+        <h1 className=' mt-8 translate-x-6 text-xl '>{heading}</h1>
+        <p className=' mt-4 translate-x-6 text-slate-900 max-w-[80%]'>{desc}</p>
     </div>
   )
 }
--- pages/Browse.js
+++ pages/Browse.js
@@ -5,13 +5,17 @@ import TopBar from '../components/TopBar';
 import MainContent from '../components/MainContent'; // Corrected import path
 import Card from '../components/common/Card';
 import {FaHashtag} from "react-icons/fa"
+import Favorites from '../components/Template/Favorites';
+import FreshTemplate from '../components/Template/FreshTemplate';
 const Browse = () => {
   return (
-    <div className="flex h-screen ml-20 overflow-hidden bg-gray-100">
+    <div className="flex  h-fit ml-20 overflow-hidden bg-gray-100">
       <Sidebar />
-      <div className=" flex w-[100%] flex-col border border-red-800">
+      <div className=" flex   flex-col h-fit ">
         <TopBar />
-         
+         {/*templates*/}
+         <Favorites/>
+         <FreshTemplate/>
       </div>
     </div>
   );
