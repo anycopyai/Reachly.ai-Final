d66cf72e72fa26cc68e8535370579a4118025846
abind09 <amitbind1947@gmail.com>
2024-02-07 13:50:22 +0530

    correct login checkbox

--- pages/login.js
+++ pages/login.js
@@ -216,10 +216,11 @@ function Login() {
                   <div class="flex items-center">
                     <div class="flex">
                       <input
+                      required
                         id="remember-me"
                         name="remember-me"
                         type="checkbox"
-                        class="shrink-0 mt-0.5 border-gray-200 rounded text-blue-600 pointer-events-none focus:ring-blue-500 dark:bg-gray-800 dark:border-gray-700 dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-800"
+                        // class="shrink-0 mt-0.5 border-gray-200 rounded text-blue-600 pointer-events-none focus:ring-blue-500 dark:bg-gray-800 dark:border-gray-700 dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-800"
                       />
                     </div>
                     <div class="ms-3">
