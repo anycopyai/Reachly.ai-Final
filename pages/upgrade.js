8d156cd09f77bbdbaed4a64f12967bdd634eed54
pankaj <codepankaj1983@gmail.com>
2024-03-19 17:36:55 +0530

    price page responsive

--- pages/upgrade.js
+++ pages/upgrade.js
@@ -71,45 +71,6 @@ const priceCard = [
   },
 ];
 
-// const basicList = [
-//   { text: "30+ Templates" },
-//   { text: "50 Credits per month" },
-//   { text: "Writer with 7-Days Trial" },
-//   { text: "Access to 30+ templates" },
-//   { text: "URL to Ad feature (Limited)" },
-//   { text: "Chrome Extension" },
-//   { text: "Desktop App (Limited Features)" },
-//   { text: "Built-in Grammarly" },
-//   { text: "Access to writer" },
-// ];
-
-// const standardList = [
-//   { text: "300 Credits per month" },
-//   { text: "Access to 45+ templates" },
-//   { text: "3 Projects" },
-//   { text: "Writer" },
-//   { text: "URL to Ad feature (Enhanced)" },
-//   { text: "Chrome Extension" },
-//   { text: "Full Desktop App" },
-//   { text: "Built-in Grammarly" },
-//   { text: "Priority Support" },
-//   { text: "Community Access" },
-// ];
-
-// const proList = [
-//   { text: "Unlimited Credits" },
-//   { text: "Access to 90+ templates" },
-//   { text: "Unlimited Projects" },
-//   { text: "Writer" },
-//   { text: "URL to Ad feature (Advanced Workflow)" },
-//   { text: "Chrome Extension" },
-//   { text: "Full Desktop App" },
-//   { text: "Built-in Grammarly" },
-//   { text: "Copy Insights (Coming soon)" },
-//   { text: "Priority Support" },
-//   { text: "Community Access" },
-// ];
-
 const Upgrade = () => {
   const router = useRouter();
   return (
@@ -150,22 +111,41 @@ const Upgrade = () => {
         </div>
       </div>
 
-      <div className="grid md:grid-cols-3 w-3/6 mx-auto mt-10 mb-10 border border-[#00000040]">
+      <div className="grid md:grid-cols-3 md:w-4/6 mx-auto gap-4 md:gap-0 mt-10 mb-10 md:border md:border-[#00000040]">
         {priceCard?.map((item, index) => {
           return (
             <div
-              className="border-r-1 last:border-none border-[rgba(0,0,0,0.25)] py-6 px-8 flex flex-col gap-4"
+              className="border md:border-0 md:border-r-1 md:last:border-none border-[rgba(0,0,0,0.25)] py-6 px-10 pb-12 flex flex-col gap-4"
               key={index}
             >
               <div>
-                <h4 className="text-xl text-black mb-6 font-medium">
+                <h4 className="text-xl text-black mb-6 font-medium flex items-center gap-2">
+                  {item.planType === "Standard" && (
+                    <img src="images/icon-star-green.svg" alt="star" />
+                  )}
                   {item.type}
+                  {item.planType === "Standard" && (
+                    <span className="bg-[#EAB308] text-xs text-white px-2 py-1 rounded-sm flex items-center justify-center">
+                      Most Popular
+                    </span>
+                  )}
                 </h4>
-                <p className="font-medium text-black text-base">{item.price}</p>
+                <p className="font-medium text-[rgba(0,0,0,0.45)] text-base">
+                  {item.price}
+                </p>
               </div>
               <div>
                 <Button
-                  className={`bg-[#F5F5F5] border border-[#D9D9D9] px-6 py-2 text-base text-[rgba(0,0,0,0.25)] h-auto`}
+                  className={`border px-6 py-2 text-base h-auto w-full font-bold ${
+                    item.btnType === "disabled" &&
+                    "bg-[#F5F5F5] border-[#D9D9D9] hover:!border-[#D9D9D9] text-[#00000040] hover:!text-[#00000040]"
+                  } ${
+                    item.btnType === "success" &&
+                    "bg-[#34A853] border-[#34A853] text-white hover:!text-white hover:!border-[#34A853]"
+                  } ${
+                    item.btnType === "info" &&
+                    "bg-navblue border-navblue text-white"
+                  }`}
                 >
                   {item.btnText}
                 </Button>
@@ -183,8 +163,8 @@ const Upgrade = () => {
                   );
                 })}
               </ul>
-              <h4 className="text-sm text-[#323232]">
-                {item.actionText}{" "}
+              <h4 className="text-sm text-[#323232] flex gap-1">
+                {item.actionText}
                 <Link href="/">
                   <a className="underline font-semibold">{item.actionLink}</a>
                 </Link>
