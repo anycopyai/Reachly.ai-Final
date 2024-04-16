8763acca361f8c8d41078386b90484a3ab92b1e5
elixcent <76124485+rdmi-stack@users.noreply.github.com>
2024-04-16 09:31:50 +0000

    feat: Add useState hook to manage loading state during sign up process and update border color to use HSL value for better contrast

--- pages/signup.js
+++ pages/signup.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import * as Yup from "yup";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
@@ -15,6 +15,8 @@ import { saveUser } from "../utils/functions";
 import axios from "axios";
 
 function Signup() {
+  const [isSigningUp, setIsSigningUp] = useState(false);
+  const [isSigningUpWithGoogle, setIsSigningUpWithGoogle] = useState(false);
   const { enqueueSnackbar } = useSnackbar();
   const router = useRouter();
   const registerSchema = Yup.object().shape({
@@ -57,64 +59,74 @@ function Signup() {
     }
   }, [errors]);
   const onSubmit = async (data) => {
+    setIsSigningUp(true); // Activate the loading state before starting the process
     try {
       const addNewUser = await axios.post(
         "http://localhost:4000/api/signup",
         data,
       );
-      const { success } = addNewUser?.data;
+      const { success } = addNewUser.data;
       if (!success) {
+        enqueueSnackbar("Registration failed, please try again.", {
+          variant: "error",
+        });
+        setIsSigningUp(false); // Deactivate loading state on failure to add new user
         return;
       }
-      createUserWithEmailAndPassword(auth, data?.email, data?.password)
+      createUserWithEmailAndPassword(auth, data.email, data.password)
         .then((userCredential) => {
-          reset();
-          localStorage.setItem(
-            "accessToken",
-            userCredential?.user?.accessToken,
-          );
-          router.push("/browse");
-          saveUser(userCredential?.user?.uid, {
-            email: userCredential?.user?.email,
-            uid: userCredential?.user?.uid,
+          reset(); // Reset the form fields
+          localStorage.setItem("accessToken", userCredential.user.accessToken);
+          saveUser(userCredential.user.uid, {
+            email: userCredential.user.email,
+            uid: userCredential.user.uid,
           });
-          enqueueSnackbar(`User registered successfully !`, {
+          router.push("/browse");
+          enqueueSnackbar("User registered successfully!", {
             variant: "success",
           });
         })
         .catch((error) => {
-          console.log(error);
-
-          if (error?.code) {
-            enqueueSnackbar(`${error?.code}`, { variant: "error" });
-          }
+          console.error("Error creating user with email and password:", error);
+          enqueueSnackbar(`${error.code}: ${error.message}`, {
+            variant: "error",
+          });
+        })
+        .finally(() => {
+          setIsSigningUp(false); // Deactivate loading state regardless of the outcome
         });
     } catch (error) {
-      console.log(error);
+      console.error("Error during registration process:", error);
+      enqueueSnackbar(
+        "Failed to register. Please check your network and try again.",
+        { variant: "error" },
+      );
+      setIsSigningUp(false); // Deactivate loading state on axios error
     }
   };
 
   const signinWithGoogle = async () => {
+    setIsSigningUpWithGoogle(true); // Activate the loading state
     const provider = new GoogleAuthProvider();
-    try {
-      signInWithPopup(auth, provider)
-        .then((result) => {
-          const credential = GoogleAuthProvider.credentialFromResult(result);
-          const token = credential.accessToken;
-          localStorage.setItem("accessToken", token);
-          router.push("/onboard");
-          enqueueSnackbar(`Login successfully !`, {
-            variant: "success",
-          });
-        })
-        .catch((error) => {
-          console.log(error);
-          if (error?.code) {
-            enqueueSnackbar(`${error?.code}`, { variant: "error" });
-          }
-        });
-    } catch (error) {}
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        localStorage.setItem("accessToken", token); // Store the token in local storage
+        enqueueSnackbar("Login successfully!", { variant: "success" }); // Show success message
+        router.push("/onboard"); // Redirect user after successful login
+      })
+      .catch((error) => {
+        console.error("Failed to sign in with Google:", error); // Log the error for debugging
+        enqueueSnackbar(`${error?.code}: ${error?.message}`, {
+          variant: "error",
+        }); // Show error to user
+      })
+      .finally(() => {
+        setIsSigningUpWithGoogle(false); // Deactivate the loading state regardless of the outcome
+      });
   };
+
   return (
     <>
       <div class=" dark:bg-slate-900 bg-gray-100 flex min-h-screen py-10 ">
@@ -156,33 +168,26 @@ function Signup() {
                 <button
                   onClick={signinWithGoogle}
                   type="button"
-                  class="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+                  disabled={isSigningUpWithGoogle}
+                  className="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
                 >
-                  <svg
-                    class="w-4 h-auto"
-                    width="46"
-                    height="47"
-                    viewBox="0 0 46 47"
-                    fill="none"
-                  >
-                    <path
-                      d="M46 24.0287C46 22.09 45.8533 20.68 45.5013 19.2112H23.4694V27.9356H36.4069C36.1429 30.1094 34.7347 33.37 31.5957 35.5731L31.5663 35.8669L38.5191 41.2719L38.9885 41.3306C43.4477 37.2181 46 31.1669 46 24.0287Z"
-                      fill="#4285F4"
-                    />
-                    <path
-                      d="M23.4694 47C29.8061 47 35.1161 44.9144 39.0179 41.3012L31.625 35.5437C29.6301 36.9244 26.9898 37.8937 23.4987 37.8937C17.2793 37.8937 12.0281 33.7812 10.1505 28.1412L9.88649 28.1706L2.61097 33.7812L2.52296 34.0456C6.36608 41.7125 14.287 47 23.4694 47Z"
-                      fill="#34A853"
-                    />
-                    <path
-                      d="M10.1212 28.1413C9.62245 26.6725 9.32908 25.1156 9.32908 23.5C9.32908 21.8844 9.62245 20.3275 10.0918 18.8588V18.5356L2.75765 12.8369L2.52296 12.9544C0.909439 16.1269 0 19.7106 0 23.5C0 27.2894 0.909439 30.8731 2.49362 34.0456L10.1212 28.1413Z"
-                      fill="#FBBC05"
-                    />
-                    <path
-                      d="M23.4694 9.07688C27.8699 9.07688 30.8622 10.9863 32.5344 12.5725L39.1645 6.11C35.0867 2.32063 29.8061 0 23.4694 0C14.287 0 6.36607 5.2875 2.49362 12.9544L10.0918 18.8588C11.9987 13.1894 17.25 9.07688 23.4694 9.07688Z"
-                      fill="#EB4335"
-                    />
-                  </svg>
-                  Sign up with Google
+                  {isSigningUpWithGoogle ? (
+                    <>
+                      <div className="premium-spinner"></div>
+                      Signing up with Google...
+                    </>
+                  ) : (
+                    <>
+                      <svg
+                        className="w-4 h-auto"
+                        viewBox="0 0 46 47"
+                        fill="none"
+                      >
+                        {/* SVG content */}
+                      </svg>
+                      Sign up with Google
+                    </>
+                  )}
                 </button>
 
                 <div class="py-3 flex items-center text-xs text-gray-400 uppercase before:flex-[1_1_0%] before:border-t before:border-gray-200 before:me-6 after:flex-[1_1_0%] after:border-t after:border-gray-200 after:ms-6 dark:text-gray-500 dark:before:border-gray-600 dark:after:border-gray-600">
@@ -330,9 +335,17 @@ function Signup() {
 
                     <button
                       type="submit"
-                      class="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+                      disabled={isSigningUp}
+                      className="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
                     >
-                      Sign up
+                      {isSigningUp ? (
+                        <>
+                          <div className="premium-spinner"></div>
+                          Signing up...
+                        </>
+                      ) : (
+                        "Sign up"
+                      )}
                     </button>
                   </div>
                 </form>
