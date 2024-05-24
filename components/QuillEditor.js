25ddc2e86c6a7f96816cf1dbbe47d805d14c4c59
Team RDMI <info@rdmi.in>
2024-05-24 18:12:42 +0000

    dd

--- components/QuillEditor.js
+++ components/QuillEditor.js
@@ -1,66 +1,94 @@
-import React, { useRef, useState } from "react";
-import { Editor } from "@tinymce/tinymce-react";
-import { Button } from "antd";
-import { PiPencilSimpleLineBold } from "react-icons/pi";
+import React, { useState } from "react";
+import dynamic from "next/dynamic";
+import "quill/dist/quill.snow.css";
+
+const QuillNoSSRWrapper = dynamic(import("react-quill"), {
+  ssr: false,
+  loading: () => <p>Loading...</p>,
+});
+
+const editorStyles = {
+  backgroundColor: "#fff",
+  minHeight: "auto",
+  border: "none",
+};
+
+const quillContainerStyle = `
+  .ql-container {
+    border: none !important;
+    box-shadow: none !important;
+  }
+
+  .ql-editor {
+    font-size: 18px; /* Default font size */
+  }
+
+  .ql-editor h1 {
+    font-size: 36px; /* Custom font size for H1 */
+  }
+
+  .ql-editor h2 {
+    font-size: 32px; /* Custom font size for H2 */
+  }
+
+  .ql-toolbar {
+    border: none !important;
+    box-shadow: none !important;
+  }
+`;
+
+const toolbarOptions = [
+  [{ header: 1 }, { header: 2 }],
+  ["bold", "italic", "underline", "strike"],
+  [{ list: "ordered" }, { list: "bullet" }],
+  ["blockquote", "code-block"],
+  ["link", "image"],
+  ["clean"],
+  ["align", "indent"],
+  ["color"],
+  ["background"],
+  ["print"],
+];
 
 const QuillEditor = () => {
   const [editorContent, setEditorContent] = useState("");
-  const editorRef = useRef(null);
-  const log = () => {
-    if (editorRef.current) {
-      console.log(222222, editorRef.current.getContent());
-    }
+
+  const modules = {
+    toolbar: {
+      container: toolbarOptions,
+    },
   };
+
+  const formats = [
+    "header",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "list",
+    "bullet",
+    "blockquote",
+    "code-block",
+    "link",
+    "image",
+    "align",
+    "indent",
+    "color",
+    "background",
+    "print",
+  ];
+
   return (
-    <>
-      <Editor
-        apiKey="wu2f0p4jfqbc1b6k4jgq224rax1wxwoy99h6yltqgyoysvoq"
-        onInit={(evt, editor) => (editorRef.current = editor)}
-        initialValue=""
-        init={{
-          height: 500,
-          menubar: false,
-          placeholder: 'write something',
-          plugins: [
-            "advlist",
-            "autolink",
-            "lists",
-            "link",
-            "image",
-            "charmap",
-            "preview",
-            "anchor",
-            "searchreplace",
-            "visualblocks",
-            "code",
-            "fullscreen",
-            "insertdatetime",
-            "media",
-            "table",
-            "help",
-            "wordcount",
-            "importcss",
-            "autosave",
-            "save",
-            "directionality",
-            "visualchars",
-            "template",
-            "codesample",
-            "pagebreak",
-            "nonbreaking",
-            "emoticons",
-            "accordion",
-          ],
-
-          toolbar:
-            "undo redo print removeformat image link | blocks fontfamily fontsize | bold italic underline strikethrough forecolor backcolor removeformat align numlist bullist | ltr rtl | outdent indent",
-
-          content_style:
-            "body { font-family:Helvetica,Arial,sans-serif; } p { font-size: 18px; } h1 { font-size: 32px; } h2 { font-size: 28px; } h3 { font-size: 24px; } h4 { font-size: 20px; } h5 { font-size: 18px; }",
-        }}
+    <div style={editorStyles} className="m-4">
+      <style>{quillContainerStyle}</style>
+      <QuillNoSSRWrapper
+        placeholder="Start typing here..."
+        formats={formats}
+        modules={modules}
+        value={editorContent}
+        onChange={setEditorContent}
       />
-     
-    </>
+    </div>
   );
 };
 
